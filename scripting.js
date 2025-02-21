915c4384532b829944c3340e7489b709f3fc51c7
Ahmed-Raza-Khan <arahmed212214@gmail.com>
2025-02-21 17:46:05 +0500

    21/2/25 5:44pm Ahmedraza New Repo Published

---
+++ scripting.js
@@ -0,0 +1,7 @@
+// To get a Power of a number in JavaScript, we use the ** operator.
+// var value1 = 3;
+// console.log(value1 ** 3); 
+
+//To get a Square Root of a number in JavaScript, we use the ** operator then value.
+// var value1 = 3;
+// console.log(value1 ** 0.5);
